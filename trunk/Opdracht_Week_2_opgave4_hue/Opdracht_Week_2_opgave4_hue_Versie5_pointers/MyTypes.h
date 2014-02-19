45635008f432ad210a394117d13963acdbe55905
misteriepie@gmail.com <misteriepie@gmail.com@e4325cf5-b0b3-247a-f114-1d3057d447e4>
2014-02-19 05:17:48 +0000

    Nameing convetion adjusted

---
+++ trunk/Opdracht_Week_2_opgave4_hue/Opdracht_Week_2_opgave4_hue_Versie5_pointers/MyTypes.h
@@ -0,0 +1,40 @@
+/*----------------------------------------------------*/
+/* aut.     :  Patrick & Nikita                       */
+/* datum    :  17-02-2014                             */
+/* Project  :  Living Colors part 1                   */
+/*----------------------------------------------------*/
+
+
+#ifndef MYTYPES_H    // indien myTypes.h wordt gebruikt dan wordt deze ook mee gecompiled. En maar 1 keer Conditional inclusion (control the preprocessing)
+#define MYTYPES_H
+// met typedef maak je alleen een alias aan geen nieuw type
+#define DEBUG 0
+
+typedef enum RGBstate
+{
+  stateSetRedColor,
+  stateGoToYellow,
+  stateGoToGreen,
+  stateGoToCyane,
+  stateGoToBLue,
+  stateGoToMagenta,
+  stateGoToRed
+}state;
+
+typedef struct RGB
+{
+  int red;
+  int green;
+  int blue;
+}RGBcolor;
+
+const int RGBPINRED   = 9;  //PWM
+const int RGBPINGREEN = 10; //PWM
+const int RGBPINBLUE  = 11; //PWM
+const int LEDPINOUT   = 13; // used for testing the toggle function of the push button
+
+const int BUTTONPIN   = 2;  //Input pin for pauze button
+const int DELAYTIME   = 40;
+
+#endif MYTYPES_H
+
